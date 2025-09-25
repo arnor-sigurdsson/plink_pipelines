50b7c8b69191b8ff77a4a9e1023db02f7e739e6c
Arnor Sigurdsson <arnor-sigurdsson@users.noreply.github.com>
2025-09-25 09:48:27 -0400

    Add separate arguments for reading chunk size and processing (to one-hot) size, due to read arrays generally be IO expensive but needing less RAM, whereas processing is more compute/RAM heavy.

--- src/plink_pipelines/validation_functions.py
+++ src/plink_pipelines/validation_functions.py
@@ -2,4 +2,9 @@ from argparse import Namespace
 
 
 def validate_cl_args(cl_args: Namespace) -> None:
-    pass
+    if hasattr(cl_args, "process_chunk_size") and hasattr(cl_args, "read_chunk_size"):
+        if cl_args.process_chunk_size > cl_args.read_chunk_size:
+            raise ValueError(
+                f"process_chunk_size ({cl_args.process_chunk_size}) must be <= "
+                f"read_chunk_size ({cl_args.read_chunk_size})"
+            )
